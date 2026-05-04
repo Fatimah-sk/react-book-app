14f4a73d46435154c54b42e2f0f1d362f4b3a450
Fatimah-sk <fatimah-sakhnine@hotmail.com>
2026-05-04 09:49:34 +0200

    Third

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
@@ -8,7 +8,7 @@ import BookDetails from "./pages/BookDetails";
 import ErrorPage from "./pages/ErrorPage";
 import "./index.css";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
